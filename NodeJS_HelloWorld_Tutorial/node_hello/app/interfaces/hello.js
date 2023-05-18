bc50fceb68acfdb558079f8d39d2eca6337d95f4
Dominik Kocka <kocka98@gmail.com>
2023-05-18 20:04:59 +0200

    Add new starter

--- NodeJS_HelloWorld_Tutorial/node_hello/app/interfaces/hello.js
+++
@@ -1,4 +0,0 @@
-'use_strict';
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=hello.js.map
--- NodeJS_HelloWorld_Tutorial/node_hello/src/interfaces/hello.ts
+++
@@ -1,7 +0,0 @@
-'use_strict';
-
-export interface IHello
-{
-    name: string;
-    message: string;
-}
